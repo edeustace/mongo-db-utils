869bab175744c1764cc63d35a21541db12d08206
edeustace <ed.eustace@gmail.com>
2013-06-28 13:59:35 +0100

    fixed issue with CLI -> backup backup_s3, bumped version

--- lib/mongo-db-utils/cli.rb
+++ lib/mongo-db-utils/cli.rb
@@ -9,44 +9,49 @@ require 'mongo-db-utils/s3'
 module MongoDbUtils
   class CLI < Thor
 
+    include MongoDbUtils
+
     desc "console", "run the interactive console @param path - path to config file"
-    def console(path = MongoDbUtils::ConfigLoader::CONFIG_LOCATION)
+    def console(path = ConfigLoader::CONFIG_LOCATION)
 
       if File.extname(path) != ".yml"
         puts "Error: You must use a yaml file as your config file location"
       else
-        @loader = MongoDbUtils::ConfigLoader.new(path)
-        @config = @loader.config
-        console = MongoDbUtils::Console.new(@config, MongoDbUtils::Cmd)
+        config = get_config(path)
+        console = Console.new(config, Cmd)
         console.run
       end
     end
 
     desc "backup MONGO_URI", "backup a db with a mongo uri eg: mongodb://user:pass@server:port/dbname"
     def backup(mongo_uri, replica_set_name = nil)
-      @config = MongoDbUtils::ConfigLoader.load
-
+      config = get_config
       db = get_db(mongo_uri, replica_set_name)
       raise "can't parse uri" if db.nil?
-      MongoDbUtils::Cmd.backup(db, @config.backup_folder)
+      Cmd.backup(db, config.backup_folder)
     end
-
 
     desc "backup_s3 MONGO_URI BUCKET ACCESS_KEY SECRET_ACCESS_KEY", "backup a db to Amason s3 with a mongo uri eg: mongodb://user:pass@server:port/dbname"
     def backup_s3(mongo_uri, bucket_name, access_key_id, secret_access_key, replica_set_name = nil)
-      @config = MongoDbUtils::ConfigLoader.load
-      backup_folder = @config.backup_folder
+      config = get_config
+      backup_folder = config.backup_folder
       db = get_db(mongo_uri, replica_set_name)
       raise "can't parse uri" if db.nil?
-      MongoDbUtils::Cmd.backup_s3(backup_folder, db, bucket_name, access_key_id, secret_access_key)
+      Cmd.backup_s3(backup_folder, db, bucket_name, access_key_id, secret_access_key)
     end
 
     private
+
+    def get_config(path = ConfigLoader::CONFIG_LOCATION)
+      loader = ConfigLoader.new(path)
+      loader.config
+    end
+
     def get_db(uri, name = nil)
       if(name.nil?)
-        MongoDbUtils::Model::Db.new(uri)
+        Model::Db.new(uri)
       else
-        MongoDbUtils::Models::ReplicaSetDb.new(uri, name)
+        Model::ReplicaSetDb.new(uri, name)
       end
     end
 
--- lib/mongo-db-utils/version.rb
+++ lib/mongo-db-utils/version.rb
@@ -1,4 +1,4 @@
 module MongoDbUtils
-  VERSION = "0.0.9.3"
+  VERSION = "0.1.0"
   READY_FOR_USE = "(Beta version)"
 end
